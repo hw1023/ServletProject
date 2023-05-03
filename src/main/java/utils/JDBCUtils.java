26c9880c457ed050a0f0bfaa44753327bda04893
hw1023 <97645097+hw1023@users.noreply.github.com>
2023-05-03 12:23:50 -0400

    Update JDBCUtils.java

--- src/main/java/utils/JDBCUtils.java
+++ src/main/java/utils/JDBCUtils.java
@@ -10,7 +10,7 @@ public class JDBCUtils {
 
 	private static String jdbcURL = "jdbc:mysql://localhost:3306/mydatabase?serverTimezone=UTC";
 	private static String jdbcUsername = "root";
-	private static String jdbcPassword = "Wang1023*";
+	private static String jdbcPassword = "root";
 
 	public static Connection getConnection() {
 		Connection connection = null;
